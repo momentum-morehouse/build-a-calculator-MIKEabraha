ff43c2348e16a242c0e28180b2c9f3ff11447de2
Michael  Abraha <m.b.abraha@msmary.edu>
2020-06-15 02:47:52 +0000

    colors

--- main.js
+++ main.js
@@ -1,19 +1,16 @@
 
-function dis (value)
-{
-document.getElementById("edu").value+=value
-;}
-
-function solve()
-{
-let x = document.getElementById("edu").value
-let y = eval(x)
-document.getElementById ("edu").value = y
+function dis (value){
+document.getElementById("edu").value+=value;
 }
 
-function clr()
-{
-document.getElementById("edu").value = ""
+function solve(){
+let x = document.getElementById("edu").value;
+let y = eval(x);
+document.getElementById("edu").value = y;
+}
+
+function clr(){
+document.getElementById("edu").value = "";
 
 
 }
